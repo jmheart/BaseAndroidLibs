e6dcfcd3dfbaf3c823a69f78effd18766c96debc
BaseAndroidLibs <a770161055@qq.com>
2015-08-05 10:46:53 +0800

    添加listview

---
+++ src/com/jmheart/view/listview/BaseListview.java
@@ -0,0 +1,13 @@
+package com.jmheart.view.listview;
+
+import android.content.Context;
+import android.widget.ListView;
+
+public class BaseListview extends ListView {
+
+	public BaseListview(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
